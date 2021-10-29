83f98c5323a213c0ee0960330cdfa9a8e80c8188
Andy <andy@Andys-Air.hitronhub.home>
2021-10-29 01:24:47 -0400

    added contact form

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
+import { ContactForm } from "../contact/contact";
 import Navbar from "../Navbar";
 
 export default function Hero() {
-  let backgroundImageUrl =
-    "https://lh5.googleusercontent.com/p/AF1QipNOx4BYt3Fde9Q9VmuE6_LYKYjoskPxSJkLPfTQ=s515-k-no";
+  let backgroundImageUrl = "./image1.jpeg";
 
   let backgroundImageUrl2 =
     "https://images.unsplash.com/photo-1457131760772-7017c6180f05?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ)";
@@ -22,36 +22,29 @@ export default function Hero() {
           className="min-h-screen hero-image bg-center bg-cover flex"
           style={heroStyle}
         >
-          <div
-            className="min-h-screen hero-image bg-right-bottom bg-cover flex"
-            style={heroStyle2}
-          >
-            <div className="relative container mx-auto p-4 flex items-end z-10">
-              <div>
-                <div className="content float-left py-4 px-5 my-5">
-                  <div className="heading mb-3 text-2xl md:text-4xl">
-                    Affordable year-round care. No insurance required!
-                  </div>
-                  <div className="text leading-normal hidden sm:block">
-                    Helping Women Meet All of their Health Care Needs
-                  </div>
-                </div>
-                <div className="cta clear-left px-5">
-                  <a
-                    className="
+          <div class="container mx-auto px-6">
+            <h2 class="text-4xl font-bold mb-2 text-white">
+              Let use build your next dream Kitchen or Bathroom
+            </h2>
+          </div>
+          <div className="relative container p-4 flex items-end z-10">
+            <div>
+              <ContactForm />
+              <div className="content float-left py-4 px-5 my-5"></div>
+              <div className="cta clear-left px-5">
+                <a
+                  className="
                 no-underline
                 btn btn-primary 
                 block sm:inline-block global-transition
                 text-white"
-                    href=""
-                    target=""
-                  >
-                    <div className="flex justify-center items-center">
-                      <span>Join Alpha Today</span>
-                      <span className="fa fa-xs fa-arrow-right ml-4"></span>
-                    </div>
-                  </a>
-                </div>
+                  href=""
+                  target=""
+                >
+                  <div className="flex justify-center items-center">
+                    <span className="fa fa-xs fa-arrow-right ml-4"></span>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
