7d8f6c08fa6390c7cf21039bec60c5731e7921ec
Andy <andy@Andys-MacBook-Air.local>
2021-11-04 04:01:10 -0400

    clean up

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero/Hero";
 import About from "../components/about/about";
 export default function Home() {
