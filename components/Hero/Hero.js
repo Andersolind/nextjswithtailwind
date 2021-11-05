38737a3c7a65f6471dc83fcaf260b597666bdd4b
Andy <andy@Andys-MacBook-Air.local>
2021-11-04 22:38:00 -0400

    removed sticky header

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,4 +1,3 @@
-
 import Navbar from "../Navbar/Navbar";
 
 export default function Hero() {
@@ -14,24 +13,14 @@ export default function Hero() {
 
   return (
     <>
-      <div className="bg-white-200 sticky top-0">
+      <div className="bg-white-200 ">
         <Navbar></Navbar>
       </div>
       <section className="bg-red-200 text-green-900 relative">
         <div
-          className="min-h-screen hero-image  bg-center bg-cover flex"
+          className="min-h-screen   bg-center bg-cover flex"
           style={heroStyle}
         >
-          <div style={setOverlay} className="container mx-auto px-6">
-            <h2 className="text-4xl font-bold mb-2 text-white ">
-              Let us build your next dream Kitchen or Bathroom. Our Kitchen
-              specialists tailor fit your Ikea dreams and make them real.
-            </h2>
-            <h1 className="text-4xl font-bold mb-2 text-white">
-              Toronto Kitchen and Bathroom specialists. Let our team gives you a
-              step by step process inorder to acheive your dream renovation.
-            </h1>
-          </div>
           <div className="relative container p-4 flex items-end z-10">
             <div>
               <div className="content float-left py-4 px-5 my-5"></div>
--- components/about/about.js
+++ components/about/about.js
@@ -8,7 +8,7 @@ const About = () => {
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
         <div className="lg:px-4 lg:mt-12 ">
           <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-            How do we start building your dream kitchen,
+            How can we help you plan your dream kitchen,
           </h1>
           <div className="mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
@@ -31,6 +31,33 @@ const About = () => {
             height={250}
             placeholder="blur"
           />
+        </div>
+      </div>
+      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+        <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+          <Image
+            src={profile}
+            alt="Profile"
+            priority={true}
+            className="rounded-full"
+            width={250}
+            height={250}
+            placeholder="blur"
+          />
+        </div>
+
+        <div className="lg:px-4 lg:mt-12 ">
+          <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+            2. Make Room For What You Need,
+          </h1>
+          <div className="mt-6 text-gray-800 dark:text-white">
+            <p className="mb-4">
+              In the bathroom or kitchen we all need space Do you want to
+              dedicate space to a second deep sink — or would you rather have an
+              amazing custom counter? Careful planning will allow you to design
+              a space that works for you.
+            </p>
+          </div>
         </div>
       </div>
     </div>
