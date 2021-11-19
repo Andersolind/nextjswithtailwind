82b0aa7a4f1e46249aad87e6dada1bf76f447bcb
Andy <andy@Andys-MacBook-Air.local>
2021-11-19 14:43:27 -0500

    typo

--- components/About/AboutUs.js
+++ components/About/AboutUs.js
@@ -10,11 +10,11 @@ export default function About() {
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
         <div className="lg:px-4 lg:mt-12 ">
           <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-            CISZKOCONTRACTING How can help you plan your dream Ikea kitchen,
+            CISZKOCONTRACTING How can we help you plan your dream Ikea kitchen,
           </h1>
           <div className="mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
-              1. Start With what makes sense ! It’s really important to think
+              1. Start With what makes sense! It’s really important to think
               about how time is going to be spent in your newly renovated Ikea
               kitchen planner. If you know you like to eat at in the
               middle(island), think about sharable space as well. Do you always
